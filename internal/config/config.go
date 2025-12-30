ee8107abdd044dcba154371f81646b6336bea215
Carlos Nunez <josephxcharly@gmail.com>
2025-12-30 08:30:39 -0700

    Add database connection retries on startup

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,37 +1,57 @@
 package config
 
 import (
-	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"log/slog"
 	"os"
 	"time"
 )
 
+const MAX_RETRIES = 3
+
 func InitDataBase() (*sql.DB, error) {
 	databaseUri := os.Getenv("DB_URI")
 
+	var (
+		conn *sql.DB
+		err error
+	)
+	
 	if databaseUri == "" {
 		return nil, errors.New("DB_URI is not set")
 	}
+
 	slog.Info("Initializing database connection")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	for i := range MAX_RETRIES {
+		conn, err = sql.Open("postgres", databaseUri)
+		if err == nil {
+			err = conn.Ping()
+		}
 
-	conn, err := sql.Open("postgres", databaseUri)
+		if err == nil {
+			break
+		}
+
+		slog.Warn(
+			"Cannot connect to database, retrying",
+			"attempt", i,
+			"max", MAX_RETRIES,
+			"error", err,
+		)
+
+		time.Sleep(1 * time.Second)
+	}
+
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("database unavailable after %d attempts: %w", MAX_RETRIES, err)
 	}
 
-	if err := conn.PingContext(ctx); err != nil {
-		return nil, err
-	}
-	
 	conn.SetMaxOpenConns(5)
 	conn.SetMaxIdleConns(2)
-	
+
 	slog.Info("Database connected successfully")
 
 	return conn, nil
